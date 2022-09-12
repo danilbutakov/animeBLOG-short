5880c5cbd19d42162c487635e50c7798fa53d328
danilbutakov <12021109359@irgups.ru>
2022-09-12 20:40:12 +0800

    Вернул все назад6

--- public/popup.php
+++ public/popup.php
@@ -1,7 +1,13 @@
 <?php
 session_start();
 
-require_once __DIR__ . '../../vendor/connect.php';
+//require_once __DIR__ . '../../vendor/connect.php';
+
+$connect = mysqli_connect("localhost", "phpadmin", "password", "anime-blog");
+
+if (!$connect) {
+    die('Error connect to DataBase');
+}
 
 $name = $_POST['name'];
 $email = $_POST['email'];
